93b065577a3f8048464d08959082151991efaf5c
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-20 19:48:25 +0900

    edit coorinate controller

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -5,7 +5,15 @@ class CoordinatesController < ApplicationController
   end
 
   def new
-    
+    @coordinate = Coordinate.new
+  end
+  
+  def create
+    redirect_to user_path(current_user.id)
+  end
+
+  def show
+
   end
 
 end
