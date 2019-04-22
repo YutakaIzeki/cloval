73d02b34d27aa24338519442cb3d8cfda95796d3
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-22 19:27:15 +0900

    put in coordinates to instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    @number = params[:id]
+    # ビューでpopを使うためにAcctiveRecord配列からrubyの配列に入れ直し
+    @coordinates = []
+    current_user.coordinates.each do |coordinate|
+      @coordinates << coordinate
+    end
   end
 end
