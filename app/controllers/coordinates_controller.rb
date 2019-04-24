df151356ba3a3c8e55d42409c47a253a0712aaef
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-24 20:23:48 +0900

    show Average in top page

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -2,11 +2,19 @@ class CoordinatesController < ApplicationController
 
   def index
     @coordinates = []
+    @coordinate_average_arry = []
 
     params[:kind].present? ? coordinates = Coordinate.where(sex: params[:kind]) : coordinates = Coordinate.all
 
     coordinates.each do |coordinate|
       @coordinates << coordinate
+
+      # コーディネートの評価の平均値を計算し、配列に格納
+      if coordinate.evaluation_number == 0 
+        @coordinate_average_arry << 0
+      else
+        @coordinate_average_arry << (coordinate.evaluation_value.to_f / coordinate.evaluation_number.to_f).round(1)
+      end
     end
   end
 
@@ -24,6 +32,10 @@ class CoordinatesController < ApplicationController
   end
 
   def update
+    coordinate = Coordinate.find(params[:id])
+    coordinate.evaluation_value += params[:add_point].to_i
+    coordinate.evaluation_number += 1
+    coordinate.save
     redirect_to root_path
   end
 
