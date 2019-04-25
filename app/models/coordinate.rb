3bfe6bd80f51e31d192d3728571cbb810fba2818
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-25 17:30:12 +0900

    Show user rank

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -7,6 +7,17 @@ class Coordinate < ApplicationRecord
     self.evaluation_value += add_point.to_i
     self.evaluation_number += 1
     self.user.score += add_point.to_i
+
+    # ユーザーのランクを判定
+    case self.user.score
+    when 0..5
+      self.user.rank = 3
+    when 5..20
+      self.user.rank = 2
+    else
+      self.user.rank = 1
+    end
+
     self.user.save
     self.save    
   end
