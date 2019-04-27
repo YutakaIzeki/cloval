e17182f41f6889537a43519a601e486d9e73c41c
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-27 16:01:13 +0900

    Description delated to turbolinks in JS file

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,26 +1,32 @@
-$(function(){
-  $('#new_coordinate').on('change', function(e){
-    var file = e.target.files[0],
-        reader = new FileReader(),
-        $preview = $(".preview");
-        // t = this
 
-    // if(file.type.index0f("image") < 0){
-    //   return false;
-    // }
+$(document).on('turbolinks:load', function(){
+  
+  $(function(){
+    $('#new_coordinate').on('change', function(e){
+      var file = e.target.files[0],
+      reader = new FileReader(),
+      $preview = $(".preview");
+      
+      t = this
+      
+      // if(file.type.index0f("image") < 0){
+        //   return false;
+        // }
+        
+        reader.onload = (function(file) {
+          return function(e) {
+            $preview.empty();
+            $preview.append($('<img>').attr({
+              src: e.target.result,
+              width: '361px',
+              class: "mt-5 mb-4 img-fluid",
+              title: file.name
+            }));
+          }
+        })(file);
+        
+        reader.readAsDataURL(file);
+      })
+    })
 
-    reader.onload = (function(file) {
-      return function(e) {
-        $preview.empty();
-        $preview.append($('<img>').attr({
-          src: e.target.result,
-          width: '361px',
-          class: "mt-5 mb-4 img-fluid",
-          title: file.name
-        }));
-      }
-    })(file);
-
-    reader.readAsDataURL(file);
-  })
 })
