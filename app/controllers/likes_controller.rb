2984829534917a7070e63a30a39d3661d4e24b61
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-28 17:38:08 +0900

    Like create and destroy in controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,14 @@
 class LikesController < ApplicationController
+
+  before_action :logged_in_user
+
   def create
+    @coordinate = Coordinate.find(params[:id])
+    @coordinate.like(current_user)
   end
 
   def destroy
+    @coordinate = Like.find(params[:id]).coordinate
+    @coordinate.unlike(current_user)
   end
 end
