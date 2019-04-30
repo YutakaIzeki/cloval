a8f4ab1948baedf74268352a6d243bf21dbfebdc
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-30 18:53:49 +0900

    Modify validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,5 +4,5 @@ class Like < ApplicationRecord
   belongs_to :coordinate
 
   validates :user_id, presence: true
-  validates :micropost_id, presence: true
+  validates :coordinate_id, presence: true
 end
