7c3ce456283c6adec1c5ef2c8a56149ef6d49aba
Yutaka Izeki <einegitarreund@gmail.com>
2019-04-30 16:16:33 +0900

    Edit likes controller
    いいねを保存すアクション、いいねを削除するアクションを作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,24 @@ class LikesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    @coordinate = Coordinate.find(params[:id])
-    @coordinate.like(current_user)
+    binding.pry
+    @coordinate = Coordinate.find(params[:coordinate_id])
+    unless @coordinate.like?(current_user)
+      @coordinate.like(current_user)
+      respond_to do |format|
+        format.html{ redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 
   def destroy
     @coordinate = Like.find(params[:id]).coordinate
-    @coordinate.unlike(current_user)
-  end
+    if @coordinate.like?(current_user)
+      @coordinate.unlike(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
 end
